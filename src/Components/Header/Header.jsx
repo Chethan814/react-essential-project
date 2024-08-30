ab28a1707c8cdc1a000976d19ca4306cadfd0990
chethan <chethanchethu99339@gmail.com>
2024-08-30 14:19:06 +0530

    added the dynamic feature to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,26 @@
-export default function Header() {
+import React from "react";
+import conceptsImage from '../../assets/react-core-concepts.png'
+
+const reactdescription = ['Fundamental', 'Core', 'Crucial']
+
+function genRandomInt(max){
+    return Math.floor(Math.random() * ( max + 1 ));
+}
+
+export default function () {
+    let description = genRandomInt(2)
+    
+
   return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
+    <>
+      <header>
+        <img src={conceptsImage} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {reactdescription[description]} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    </>
   );
 }
