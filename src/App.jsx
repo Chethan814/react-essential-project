e0e9f767e1623456794bc080cf150c16b7232bef
chethan <chethanchethu99339@gmail.com>
2024-08-30 19:24:25 +0530

    created the card section and learn how to props from other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+// import CoreComponent from "./Components/CoreComponenets/CoreComponent";
 import Header from "./Components/Header/Header";
+import { CoreConcept } from "./Components/CoreConcept/CoreConcept";
+import { CORE_CONCEPTS } from "./Components/CoreConcept/Data.jsx"
 
 function App() {
   return (
     <div>
-      
       <Header />
 
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts </h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        
       </main>
     </div>
   );
---
+++ src/Components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,13 @@
+
+
+export function CoreConcept({ image ,  description,title }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+
