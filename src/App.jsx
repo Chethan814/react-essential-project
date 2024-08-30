5a79fd90609a1e0436efb8c81e1ceb29b28676e3
chethan <chethanchethu99339@gmail.com>
2024-08-30 14:18:01 +0530

    set up the project with vite and style

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      
+      <Header />
+
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
