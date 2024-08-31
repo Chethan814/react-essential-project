e63fa0032f454b036a3e191301e35de7f36e99a9
chethan <chethanchethu99339@gmail.com>
2024-08-31 19:09:58 +0530

    added the example section and learn to manage the use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-// import CoreComponent from "./Components/CoreComponenets/CoreComponent";
+import { useState } from "react";
 import Header from "./Components/Header/Header";
 import { CoreConcept } from "./Components/CoreConcept/CoreConcept";
-import { CORE_CONCEPTS } from "./Components/CoreConcept/Data.jsx"
+import { CORE_CONCEPTS, EXAMPLES } from "./Components/CoreConcept/Data.jsx";
+import TabButton from "./Components/TabButton/TabButton.jsx";
 
 function App() {
+  let [content, setContent] = useState("components");
+  function tabClick(SelectedBtt) {
+    // console.log(setContent , SelectedBtt);
+    setContent(SelectedBtt);
+  }
+
   return (
     <div>
       <Header />
@@ -23,7 +30,26 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => tabClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => tabClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => tabClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => tabClick("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[content].title}</h3>
+            <p>{EXAMPLES[content].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[content].code}
+              </code>
+            </pre>
+          </div>
+        </section>
       </main>
     </div>
   );
